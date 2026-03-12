05cb234b6195485735e6000caf0863898f22aaa7
Patel Sahil R. <sahilpatel1341@gmail.com>
2026-03-12 09:51:37 +0530

    Remove firebase config and add placeholder comment
    Removed firebase configuration details and added a placeholder comment for user-defined configuration.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,10 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCsWtDEtGU5-pu6o0XZbXiC9FJlkCOw-G0",
-  authDomain: "ai-task-orchestrator-c657a.firebaseapp.com",
-  projectId: "ai-task-orchestrator-c657a",
-  storageBucket: "ai-task-orchestrator-c657a.firebasestorage.app",
-  messagingSenderId: "514839212639",
-  appId: "1:514839212639:web:0cdf86133cb0512736e1ea",
-};
+// craet your own firebase and past code here //
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
+
